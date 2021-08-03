799d43a1d61c2dbb311cabd3c84028b88501cbeb
Muna Sikawa <munasikawa@gmail.com>
2021-08-03 18:26:25 +0300

    Dynamic Server

--- client/client.js
+++ client/client.js
@@ -1,10 +1,14 @@
 console.log('Hello World!');
 
 const form = document.querySelector('form');
+const loadingElement = document.querySelector('.loading'); // a reference to the loading class (icon)
+
+loadingElement.style.display = 'none';
+
 
 form.addEventListener('submit', (event) => {
-    event.preventDefault(); // This stops the browser from trying to send the from data
-    const formData = new FormData(form); //formData is built into the web browser
+    event.preventDefault(); // This stops the browser from trying to send the formm data
+    const formData = new FormData(form); //formData is built into the web browser, it works by passing in a reference to the forms
     const name = formData.get('name'); // we get the name from the form
     const content = formData.get('content'); // we get the content from the form
 
@@ -12,6 +16,9 @@ form.addEventListener('submit', (event) => {
         name,
         content
     };
+    console.log(hoot);
+    form.style.display = 'none'; //form is hidden, once 
+    loadingElement.style.display = '';
+
 });
 
-console.log();
